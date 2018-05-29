ba7d3d4f6ee688df86232e522586c5b274a76ddb
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-29 01:01:09 -0400

    implemented monkey patch of hash class using self

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    returnArray = []
+    self.each do |parts|
+      arguments.each do |args|
+        if parts[1] == args
+          returnArray.push(parts[0])
+        end
+      end
+    end
+    returnArray
   end
 end
